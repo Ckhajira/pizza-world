3a83fc8f1f04e2e2df21e8a7b297945f449ce949
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-19 21:17:39 +0300

    This commit adds the pepperoni toppings

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ function getCrust(){
     let getCrust = document.getElementById('getcrust').value;
     return parseInt(getCrust)
 }
+
+function getPep(){
+    let Pep = 0;
+    let pep = document.getElementById('defaultInline1');
+    if (pep.checked=true){
+        Pep = 200;
+    }
+    return parseInt(Pep)
+}
