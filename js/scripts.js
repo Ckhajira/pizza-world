d6bea150e4f3dae522060e6f4252eaae6a34bab1
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-19 20:56:08 +0300

    This commit provides the user with the option to choose the crust by creating the getCrust method

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function getSize(){
     let getSize = document.getElementById('getsize').value;
     return parseInt(getSize)
 }
+
+function getCrust(){
+    let getCrust = document.getElementById('getcrust').value;
+    return parseInt(getCrust)
+}
